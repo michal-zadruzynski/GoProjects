9812c751a4e1c655c0a7eb08a807ee48f2306446
Michał Zadrużyński <michal.zadruzynski@gmail.com>
2015-09-12 12:28:26 +0200

    web server staff

--- GoProjects/test1/main.go
+++ GoProjects/test1/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 )
+
+var persons = map[string]string{
+	"87040609075": "Michał",
+	"891030":      "Paulina",
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, getPersons(persons), r.URL.Path[1:])
+}
 
 func main() {
 	fmt.Println("GoLang absolute begin")
 	rand.Seed(time.Now().Unix())
-	fmt.Printf("My favourite number is not %d", rand.Intn(10))
+	fmt.Printf("My favourite number is not %d\n", rand.Intn(10))
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func getPersons(pers map[string]string) string {
+	var names string
+	for _, v := range pers {
+		names += v + "\n"
+	}
+	return names
 }
