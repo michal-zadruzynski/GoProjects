df9e340f7b82923fd77c17378b2b9553b9e78de5
Michał Zadrużyński <michal.zadruzynski@gmail.com>
2015-09-12 11:48:04 +0200

    enhancement

--- GoProjects/test1/main.go
+++ GoProjects/test1/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Printf("GoLang absolute begin")
+	fmt.Println("GoLang absolute begin")
+	rand.Seed(time.Now().Unix())
+	fmt.Printf("My favourite number is not %d", rand.Intn(10))
 }
